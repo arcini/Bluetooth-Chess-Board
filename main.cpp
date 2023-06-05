c7f68ce739284f62f207b85e64096fd372bd4553
arcini <arcini8@gmail.com>
2023-06-05 14:21:45 -0400

    further improvements in basic hardware functioning test firmware

--- main.cpp
+++ main.cpp
@@ -1,26 +1,41 @@
 #include "mbed.h"
-#include <string>
 #include <cstdio>
-#include <vector>
+#include <string>
 
-
-BusOut mux_1_select(PA_0, PA_1, PA_2, PA_3);
+// ----- PORT INITIALIZATION -----
+BusOut mux_1_select(PB_8, PA_1, PA_2, PA_3);
 BusOut mux_2_select(PA_4, PA_5, PA_6, PA_7);
+
 // PA_9 and PA_10 are used for UART (serial monitor), so PB 6 and 7 used instead.
 BusOut mux_3_select(PA_8, PB_6, PB_7, PA_11); 
 BusOut mux_4_select(PA_12, PA_13, PA_14, PA_15);
 
 
+// ----- GLOBAL VARS -----
+// Data structures
 bool board[8][8];
+
+// Port containers
+DigitalOut mux_enables = PB_9;
 DigitalIn mux_outputs[] = {PB_0, PB_1, PB_2, PB_3};
 BusOut* mux_selectors[] = {&mux_1_select, &mux_2_select, &mux_3_select, &mux_4_select};
 
+
+// ----- FUNCTION SIGNATURES -----
+bool set_and_read_mux(int mux_index, int channel);
 void load_board();
 void print_board();
+void mux_1_slow();
+
 
 // main() runs in its own thread in the OS
 int main()
 {
+    // Make sure all muxes are enabled
+    for (int i = 0; i < 4; ++i) {
+        mux_enables.write(0);
+    }
+
     while (true) {
         // Load the state of the board
         load_board();
@@ -29,8 +44,26 @@ int main()
         print_board();
 
         // Wait for a second
-        ThisThread::sleep_for(500ms);
+        ThisThread::sleep_for(400ms);
     }
+}
+
+bool set_and_read_mux(int mux_index, int channel) {
+    // Disable the mux temporarily
+    mux_enables.write(1);
+
+    // Select the channel
+    // NOTE: Using an array of pointers is probably slow, but BusOut is nonCopyable
+    (*mux_selectors[mux_index]).write(channel);
+
+    // Re-enable the mux
+    mux_enables.write(0);
+
+    // Read the mux and return a boolean for the channel's state
+        // 0 (LL low): piece present
+        // 1 (LL high): piece not present
+        // If piece is present....
+    return mux_outputs[mux_index].read() == 0 ? true : false;
 }
 
 // Load the board state into the board 2D array
@@ -41,19 +74,11 @@ void load_board() {
     for (int i = 0; i < 4; ++i) {
         // For each possible square in the mux
         for (int s = 0; s < 16; ++s) {
-            // Set the current mux (i) to select the current square (s)
-            // NOTE: Using an array of pointers is probably slow, but BusOut is nonCopyable
-            (*mux_selectors[i]).write(s);
-            
             // Board representation math
             col = 7 - (s % 8);
             row = s >= 8 ? 2 * i + 1 : 2 * i;
-
-            // Read the value for the square and store in board array
-            // 0 (LL low): piece present
-            // 1 (LL high): piece not present
-            // If piece is present....
-            board[row][col] = mux_outputs[i].read() == 0 ? true : false;
+            
+            board[row][col] = set_and_read_mux(i, s);
         }
     }
 }
@@ -71,4 +96,40 @@ void print_board() {
         out += "\n";
     }
     printf("%s", out.c_str());
+}
+
+void mux_1_slow() {
+    for (int s = 0; s < 16; ++s) {
+        (*mux_selectors[0]).write(s);
+
+        printf("%d\n", s);
+        ThisThread::sleep_for(200ms);
+    }
+}
+
+
+void test_specific_square() {
+    printf("Enter integer of square to select (0-63)\n");
+    char square;
+    square = getchar();
+    printf("Selecting square: %d\n (q to quit)", square);
+
+    int mux_selected = square / 16;
+    int channel_selected = square % 16;
+
+    (*mux_selectors[mux_selected]).write(channel_selected);
+    ThisThread::sleep_for(20ms);
+
+    while (true) {
+        if (mux_outputs[mux_selected].read() == 0) {
+            const char * has_piece_string = mux_outputs[mux_selected].read() == 0 ? "true" : "false";
+            printf("output: %s\n", has_piece_string);
+        }
+
+        char response = getchar();
+
+        if (response == 'q') {
+            break;
+        }
+    }
 }
